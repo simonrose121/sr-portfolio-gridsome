adcea817b32d12eb33ab486f04d6620b5175013e
Simon Rose <simonrose121@gmail.com>
2021-05-12 11:35:10 +0100

    add: 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+	<Layout>
+		<div class="container">
+			<div class="flex items-center justify-center height-footer">
+				<p class="">
+					Sorry, that page could not be found.
+				</p>
+			</div>
+		</div></Layout
+	>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.height-footer {
+	height: calc(100vh - 12.5rem);
+}
+</style>
